d962086494b18d76eb84d19dd1ae7a7521183450
Kadijah Ayling <ayling.kadijah@gmail.com>
2018-11-26 23:05:15 +0000

    added more else if

--- js/script.js
+++ js/script.js
@@ -9,23 +9,32 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+
+   
+
+
+var userChoice;
+var computerChoice ;
+var winner ;
+var randomNumber = 0;
 $("#shoot").click(function(){
-    var inputValue = $("userChoice").val();
+     var inputValue = $("userChoice").val();
     $("#userChoice").text(inputValue);
-});
-$("#shoot").click(function(){
-    $("#computerChoice").text($("#").val());
-    var randomNumber = 0;
-});
-var userChoice = "";
-var computerChoice = "";
-var winner = "";
-
-
-var randomNumber = Math.random;
- if (randomNumber>.66) {
-      $("#result").html("ninja");
+    $("#computerChoice").text($("randomNumber").val());
+  
+if (randomNumber > .66) {
+   computerChoice = 'ninja'; 
+      //$("#result").html("ninja");
  }
 else if(randomNumber > .33) {
+    
     $("#result").html ("bear");
 }
+else if (randomNumber >= 0  ){
+    
+    $("#result").html ("cowboy");
+}
+$("#result").html (inputValue);
+    
+});
+ 
